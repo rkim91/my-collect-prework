a0801f7ddb3cfa2d062ff265b506aa21aa5659c9
Richard Kim <richeekim@gmail.com>
2018-04-03 00:52:07 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  i = 0
+  arr = []
+  while i < array.length
+    arr << yield(array[i])
+    i += 1
+  end
+  return arr
+end
 
-
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+# my_collect(array) do |name|
+#   name.split(" ").first
+# end
